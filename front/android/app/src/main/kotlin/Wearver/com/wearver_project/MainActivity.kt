ff7036e4c4148d6758a933a8afb9cf1e9ca018a8
Yacine-AbuKhalil <yacine.abu-khalil@epitech.eu>
2024-05-11 16:19:51 +0200

    DEL bugged proj

--- front/android/app/src/main/kotlin/Wearver/com/wearver_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package Wearver.com.wearver_project
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- front/lib/global_lib.dart
+++
@@ -1,9 +0,0 @@
-library globals;
-
-int globalInt = 0;
-bool globalBoolean = true;
-String globalString = "";
-String email = "";
-String? name = "";
-double globalDouble= 10.0;
-bool darkmode = false;
